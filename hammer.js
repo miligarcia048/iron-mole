c291c8423a6d1052c7d473c3b9519aaa251e0993
João Mesquita <joaopsmesquita@gmail.com>
2022-10-20 19:50:01 +0100

    animation hammer

--- hammer.js
+++ hammer.js
@@ -1,44 +1,54 @@
 class Hammer {
-    constructor(x, y) {
-        this.x = x + 50;
-        this.y = y - 5;
-        this.width = 80;
-        this.height = 80;
+  constructor(x, y) {
+    this.x = x + 50;
+    this.y = y - 5;
+    this.width = 80;
+    this.height = 80;
+    this.down = false;
+  }
+
+  draw() {
+    const image = new Image();
+    if (this.down) {
+      image.src = "./images/hammer2.png";
+    } else {
+      image.src = "./images/hammer.png";
     }
 
-    draw() {
-        const image = new Image();
-        image.src = "./images/hammer.png";
-        context.drawImage(image, this.x, this.y, this.width, this.height);
-    }
+    context.drawImage(image, this.x, this.y, this.width, this.height);
+  }
 
-    moveHammer(key) {
-        switch (key) {
-            case "ArrowLeft":
-                if (this.x >= 20) {
-                    this.x -= 40;
-                }
+  moveHammer(key) {
+    switch (key) {
+      case "ArrowLeft":
+        if (this.x >= 20) {
+          this.x -= 40;
+        }
 
-                break;
-            case "ArrowRight":
-                if (this.x <= 630) {
-                    this.x += 40;
-                }
-                break;
+        break;
+      case "ArrowRight":
+        if (this.x <= 630) {
+          this.x += 40;
+        }
+        break;
 
-            case "ArrowUp":
-                if (this.y >= 50) {
-                    this.y -= 40;
-                }
-                break;
-            case "ArrowDown":
-                if (this.y <= 400) {
-                    this.y += 40;
-                }
+      case "ArrowUp":
+        if (this.y >= 50) {
+          this.y -= 40;
+        }
+        break;
+      case "ArrowDown":
+        if (this.y <= 400) {
+          this.y += 40;
         }
     }
-    colision(enemy) {
-        return !(
-            this.y + this.height < enemy.y || this.y > enemy.y + enemy.height || this.x + this.width < enemy.x || this.x > enemy.x + enemy.width)
-    }
+  }
+  colision(enemy) {
+    return !(
+      this.y + this.height < enemy.y ||
+      this.y > enemy.y + enemy.height ||
+      this.x + this.width < enemy.x ||
+      this.x > enemy.x + enemy.width
+    );
+  }
 }
--- index.js
+++ index.js
@@ -9,178 +9,188 @@ const canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d");
 
 document.getElementById("start-button").onclick = () => {
-    level = document.querySelector("select").value;
-    document.getElementById("game-board").style.display = "flex";
-    document.getElementById("first-screen").style.display = "none";
-    startGame();
-    document.getElementById("score").innerHTML = currentGame.score;
-    //myBackgroundSound = new Audio('./sounds/boss_time.mp3');
-    //myBackgroundSound.play();
+  level = document.querySelector("select").value;
+  document.getElementById("game-board").style.display = "flex";
+  document.getElementById("first-screen").style.display = "none";
+  startGame();
+  document.getElementById("score").innerHTML = currentGame.score;
+  //myBackgroundSound = new Audio('./sounds/boss_time.mp3');
+  //myBackgroundSound.play();
 };
 
 document.getElementById("playAgain").onclick = () => {
-    currentGame.score = 0;
-    timeLeft = 60;
-    clearInterval(intervalId);
-    clearInterval(timer);
-    document.getElementById("first-screen").style.display = "flex";
-    document.getElementById("game-over").style.display = "none";
+  currentGame.score = 0;
+  timeLeft = 60;
+  clearInterval(intervalId);
+  clearInterval(timer);
+  document.getElementById("first-screen").style.display = "flex";
+  document.getElementById("game-over").style.display = "none";
 };
 
 const randomCoordinateHammer = Math.floor(Math.random() * 8);
-const showHammer = new Hammer(470, 330);
-const holesCoordinates = [{
-        x: 70,
-        y: 70,
-    },
-    {
-        x: 270,
-        y: 70,
-    },
-    {
-        x: 470,
-        y: 70,
-    },
-    {
-        x: 70,
-        y: 200,
-    },
-    {
-        x: 270,
-        y: 200,
-    },
-    {
-        x: 470,
-        y: 200,
-    },
-    {
-        x: 70,
-        y: 330,
-    },
-    {
-        x: 270,
-        y: 330,
-    },
-    {
-        x: 470,
-        y: 330,
-    },
+const hammer = new Hammer(470, 330);
+const holesCoordinates = [
+  {
+    x: 70,
+    y: 70,
+  },
+  {
+    x: 270,
+    y: 70,
+  },
+  {
+    x: 470,
+    y: 70,
+  },
+  {
+    x: 70,
+    y: 200,
+  },
+  {
+    x: 270,
+    y: 200,
+  },
+  {
+    x: 470,
+    y: 200,
+  },
+  {
+    x: 70,
+    y: 330,
+  },
+  {
+    x: 270,
+    y: 330,
+  },
+  {
+    x: 470,
+    y: 330,
+  },
 ];
 
 function startGame() {
-    timer = setInterval(updateTimer, 1000);
-    updateTimer();
+  timer = setInterval(updateTimer, 1000);
+  updateTimer();
 
-    currentGame = new Game();
-    intervalId = setInterval(() => {
-        updateCanvas();
-    }, 1000 / 60);
+  currentGame = new Game();
+  currentGame.hammer = hammer;
+  intervalId = setInterval(() => {
+    updateCanvas();
+  }, 1000 / 60);
 }
 
 function updateCanvas() {
-    currentGame.frames++;
-    context.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight);
-    holes();
-    currentGame.enemies.forEach((mole) => {
-        mole.draw();
-    });
+  currentGame.frames++;
+  context.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight);
+  holes();
+  currentGame.enemies.forEach((mole) => {
+    mole.draw();
+  });
 
-    mole();
-    currentGame.obstacle.forEach((hammer) => {
-        hammer.draw();
-    });
-    hammer();
+  mole();
+  currentGame.obstacle.forEach((hammer) => {
+    hammer.draw();
+  });
+  handleHammer();
 }
 
 function holes() {
-    holesCoordinates.forEach((coord) => {
-        const hole = new Hole(coord.x, coord.y);
-        hole.draw();
-    });
+  holesCoordinates.forEach((coord) => {
+    const hole = new Hole(coord.x, coord.y);
+    hole.draw();
+  });
 }
 
 function mole() {
-    switch (level) {
-        case "easy":
-            gameSpeed = 200;
-            break;
-        case "medium":
-            gameSpeed = 120;
-            break;
-        case "hard":
-            gameSpeed = 70;
-            break;
-    }
+  switch (level) {
+    case "easy":
+      gameSpeed = 200;
+      break;
+    case "medium":
+      gameSpeed = 120;
+      break;
+    case "hard":
+      gameSpeed = 70;
+      break;
+  }
 
-    if (currentGame.frames % gameSpeed === 0) {
-        const randomCoordinate = Math.floor(Math.random() * 8);
-        const showMole = new Mole(
-            holesCoordinates[randomCoordinate].x,
-            holesCoordinates[randomCoordinate].y
-        );
+  if (currentGame.frames % gameSpeed === 0) {
+    const randomCoordinate = Math.floor(Math.random() * 8);
+    const showMole = new Mole(
+      holesCoordinates[randomCoordinate].x,
+      holesCoordinates[randomCoordinate].y
+    );
 
-        currentGame.enemies = [];
-        currentGame.enemies.push(showMole);
-    }
+    currentGame.enemies = [];
+    currentGame.enemies.push(showMole);
+  }
 }
 
-function hammer() {
-    currentGame.obstacle = [];
-    currentGame.obstacle.push(showHammer);
+function handleHammer() {
+  currentGame.obstacle = [];
+  currentGame.obstacle.push(currentGame.hammer);
 }
 
 document.addEventListener("keyup", (keyboardEvent) => {
-
-    showHammer.moveHammer(keyboardEvent.key);
+  currentGame.hammer.moveHammer(keyboardEvent.key);
 });
 
 //collision
-whacSound = new Audio('./sounds/whack04-105536.mp3');
-document.addEventListener("keyup", (event) => {
-    if (event.code === "Space") {
-        detectCollision();
+whacSound = new Audio("./sounds/whack04-105536.mp3");
 
-    }
+document.addEventListener("keyup", (event) => {
+  if (event.code === "Space") {
+    currentGame.hammer.down = false;
+    currentGame.hammer.draw();
+    detectCollision();
+  }
+});
+
+document.addEventListener("keydown", (event) => {
+  if (event.code === "Space") {
+    currentGame.hammer.down = true;
+    currentGame.hammer.draw();
+  }
 });
 
 //detect collision
 function detectCollision() {
-    const hammer = currentGame.obstacle[0];
-    const crashed = currentGame.enemies.some(function(mole) {
-        return hammer.colision(mole);
-    });
+  const hammer = currentGame.obstacle[0];
+  const crashed = currentGame.enemies.some(function (mole) {
+    return hammer.colision(mole);
+  });
 
-    if (crashed) {
-        console.log("kill the mole");
-        currentGame.enemies = [];
-        mole();
-        whacSound.play();
-        currentGame.score++;
-        document.getElementById("score").innerHTML = currentGame.score;
+  if (crashed) {
+    console.log("kill the mole");
+    currentGame.enemies = [];
+    mole();
+    whacSound.play();
+    currentGame.score++;
+    document.getElementById("score").innerHTML = currentGame.score;
+  } else {
+    console.log("lost one point");
+    currentGame.score--;
+
+    if (currentGame.score === -6) {
+      gameOver();
     } else {
-        console.log("lost one point");
-        currentGame.score--;
-
-        if (currentGame.score === -6) {
-            gameOver();
-        } else {
-            document.getElementById("score").innerHTML = currentGame.score;
-        }
+      document.getElementById("score").innerHTML = currentGame.score;
     }
+  }
 }
 
 //TIMER
 function updateTimer() {
-    timeLeft = timeLeft - 1;
-    if (timeLeft > 0) document.getElementById("timer").innerHTML = timeLeft;
-    else {
-        gameOver();
-    }
+  timeLeft = timeLeft - 1;
+  if (timeLeft > 0) document.getElementById("timer").innerHTML = timeLeft;
+  else {
+    gameOver();
+  }
 }
 
 //Game over
 function gameOver() {
-    document.getElementById("final-score").innerHTML = currentGame.score;
-    document.getElementById("game-board").style.display = "none";
-    document.getElementById("game-over").style.display = "block";
+  document.getElementById("final-score").innerHTML = currentGame.score;
+  document.getElementById("game-board").style.display = "none";
+  document.getElementById("game-over").style.display = "block";
 }
